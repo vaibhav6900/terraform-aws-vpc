f856f405f46f59dbfefd20f43cffa49bb463e39b
Vaibhav Kumar <71449670+vaibhav6900@users.noreply.github.com>
2025-06-29 07:46:52 +0000

    second commit

---
+++ main.tf
@@ -0,0 +1,111 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+resource "aws_key_pair" "example" {
+  key_name = "terraform-vpc-starry"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr
+}
+
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
+# Iska matlab: "Agar koi traffic kahin bhi jaana chahta hai (internet pe), to..."
+# To us traffic ko Internet Gateway ke through bhejo.
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Yeh line ek subnet ko route table se jod rahi hai.
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_security_group" "webSg" {
+  name   = "web"
+  vpc_id = aws_vpc.myvpc.id
+
+  # from port and to port btata hai kaha se kaha tak k ports ko allow krna hai 
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+#  "Is security group ke andar jo bhi instance hai, wo kisi bhi protocol ke zariye, kisi bhi port pe, kahin bhi jaa sakta hai."
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-sg"
+  }
+}
+
+resource "aws_instance" "server" {
+  ami                    = "ami-020cba7c55df1f615"
+  instance_type          = "t2.micro"
+  key_name      = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub1.id
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+    host        = self.public_ip
+  }
+
+  # File provisioner to copy a file from local to the remote EC2 instance
+  provisioner "file" {
+    source      = "app.py"  # Replace with the path to your local file
+    destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello from the remote instance'",
+      "sudo apt update -y",  # Update package lists (for ubuntu)
+      "sudo apt-get install -y python3-pip",  # Example package installation
+      "cd /home/ubuntu",
+      "sudo apt install -y python3-flask",
+      "sudo python3 app.py &",
+    ]
+  }
+}
